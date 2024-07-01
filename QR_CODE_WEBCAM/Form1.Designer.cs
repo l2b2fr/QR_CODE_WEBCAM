447c7915c5ec1dc64fa1cdd114ea8af65befe980
Lê Minh Nam <108647533+l2b2fr@users.noreply.github.com>
2024-07-01 15:52:20 +0700

    first commit

---
+++ QR_CODE_WEBCAM/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace QR_CODE_WEBCAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtgDuLieu = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDuLieu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(266, 12);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(562, 21);
+            this.cboCamera.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(835, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bắt đầu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(506, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dữ liệu";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Chọn camera";
+            // 
+            // dtgDuLieu
+            // 
+            this.dtgDuLieu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDuLieu.Location = new System.Drawing.Point(1298, 105);
+            this.dtgDuLieu.Name = "dtgDuLieu";
+            this.dtgDuLieu.Size = new System.Drawing.Size(396, 720);
+            this.dtgDuLieu.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1706, 852);
+            this.Controls.Add(this.dtgDuLieu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboCamera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDuLieu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dtgDuLieu;
+    }
+}
+
---
+++ QR_CODE_WEBCAM/Form1.cs
@@ -0,0 +1,148 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace QR_CODE_WEBCAM
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in filterInfoCollection)
+                cboCamera.Items.Add(Device.Name);
+            if (cboCamera.Items.Count > 0)
+            {
+                cboCamera.SelectedIndex = 0;
+                videoCaptureDevice = new VideoCaptureDevice();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Bắt đầu")
+            {
+                videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+                videoCaptureDevice.NewFrame += FinalFrame_NewFrame;
+                videoCaptureDevice.Start();
+                timer1.Start();
+                button1.Text = "Dừng lại!";
+            }
+            else
+            {
+                button1.Text = "Bắt đầu";
+                if (videoCaptureDevice != null)
+                    if (videoCaptureDevice.IsRunning == true)
+                        videoCaptureDevice.Stop();
+                timer1.Stop();
+            }
+        }
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+            {
+                videoCaptureDevice?.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                BarcodeReader reader = new BarcodeReader();
+                Result result = reader.Decode((Bitmap)pictureBox1.Image);
+
+                if (result != null && result.ResultPoints.Length > 0)
+                {
+                    // Hiển thị kết quả lên label
+                    label1.Text = result.Text;
+
+                    // Tính toán tọa độ và kích thước của hình chữ nhật bao quanh mã QR
+                    int minX = int.MaxValue, minY = int.MaxValue, maxX = int.MinValue, maxY = int.MinValue;
+                    foreach (var point in result.ResultPoints)
+                    {
+                        if (point.X < minX)
+                            minX = (int)point.X;
+                        if (point.X > maxX)
+                            maxX = (int)point.X;
+                        if (point.Y < minY)
+                            minY = (int)point.Y;
+                        if (point.Y > maxY)
+                            maxY = (int)point.Y;
+                    }
+
+                    // Tính toán tọa độ tâm của hình chữ nhật
+                    int centerX = minX + (maxX - minX) / 2;
+                    int centerY = minY + (maxY - minY) / 2;
+
+                    // Tính toán diện tích của hình chữ nhật bao quanh đối tượng
+                    int width = maxX - minX;
+                    int height = maxY - minY;
+                    int area = width * height;
+
+                    // Hiển thị tọa độ tâm và diện tích trên label
+                    label1.Text = $"Tọa độ: ({centerX}, {centerY}) Diện tích chung: {area} pixels";
+
+                    // Vẽ hình chữ nhật bao quanh vùng chứa mã QR
+                    using (Graphics g = pictureBox1.CreateGraphics())
+                    {
+                        // Tính toán offset dựa trên chế độ zoom của PictureBox
+                        int offsetX = pictureBox1.SizeMode == PictureBoxSizeMode.Zoom
+                            ? (pictureBox1.Width - pictureBox1.Image.Width) / 2
+                            : 0;
+                        int offsetY = pictureBox1.SizeMode == PictureBoxSizeMode.Zoom
+                            ? (pictureBox1.Height - pictureBox1.Image.Height) / 2
+                            : 0;
+
+                        Rectangle rect = new Rectangle(minX + offsetX, minY + offsetY, width, height);
+                        using (Pen pen = new Pen(Color.Green, 10))
+                        {
+                            g.DrawRectangle(pen, rect);
+
+                            // Vẽ đường viền nét đứt màu vàng xung quanh hình chữ nhật
+                            pen.DashPattern = new float[] { 10, 10 };
+                            g.DrawRectangle(pen, rect);
+                        }
+
+                        // Hiển thị thông tin kết quả QR code gần với hình chữ nhật
+                        g.DrawString($"Tọa độ: ({centerX}, {centerY}) Diện tích chung: {area} pixels", new Font("Tahoma", 16f), Brushes.Black, new Point(rect.X - 60, rect.Y - 50));
+
+                        // Vẽ điểm tâm của đối tượng
+                        g.FillEllipse(Brushes.Red, centerX + offsetX - 3, centerY + offsetY - 3, 6, 6);
+                    }
+                    if (videoCaptureDevice.IsRunning)
+                    {
+                        videoCaptureDevice?.Stop();
+                    }
+                }
+
+            }
+
+        }
+    }
+}
